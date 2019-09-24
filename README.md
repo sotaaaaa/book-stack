aaa970957e72782a612387b51b5d5d41bfbd294d
Dũng <Leanhdungxxxx@gmail.com>
2019-09-24 10:32:19 +0700

    Fix

--- README.md
+++ README.md
@@ -5,4 +5,3 @@
   ```text
   npm install --global gulp-cli
   ```
-
--- gulpfile.js
+++ gulpfile.js
@@ -7,28 +7,28 @@ const gulpLoadPlugins = require('gulp-load-plugins');
 const $ = gulpLoadPlugins();
 
 
-gulp.task('clean', function () {
-  return del(['docs/**', 'docs/.*', '!docs'], {
-    force: true
-  });
+gulp.task('clean', function() {
+    return del(['docs/**', 'docs/.*', '!docs'], {
+        force: true
+    });
 });
-gulp.task('copy', function () {
-  return gulp.src([
-      '_book/**/*'
-    ])
-    .pipe(gulp.dest('docs'));
-});
-
-
-gulp.task('build', function (callback) {
-  // In gulp 4, you can return a child process to signal task completion
-  exec('gitbook build', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    callback();
-  });
+gulp.task('copy', function() {
+    return gulp.src([
+            '_book/**/*'
+        ])
+        .pipe(gulp.dest('docs'));
 });
 
-gulp.task('default', gulp.series('clean', 'build', 'copy', function (done) {
-  done();
+
+gulp.task('build', function(callback) {
+    // In gulp 4, you can return a child process to signal task completion
+    exec('gitbook build', function(err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        callback();
+    });
+});
+
+gulp.task('default', gulp.series('clean', 'build', 'copy', function(done) {
+    done();
 }));
